193006d017aa4f885eafeb2a08859e35ffb1d146
Juan Cevallos <cevallosjuanfrancisco@gmail.com>
2023-02-24 08:40:52 -0500

    :tada: create app

---
+++ gif-app/src/GifExpertApp.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react"
+
+const GifExpertApp = () => {
+
+    const [categories, setcategories] = useState([])
+    return (
+        <>
+            <h1>Gif Experts</h1>
+
+        </>
+    )
+}
+
+export default GifExpertApp
