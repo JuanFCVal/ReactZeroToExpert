703acf65b4232743419ed12adb7cb8d649f8d459
Juan Cevallos <cevallosjuanfrancisco@gmail.com>
2023-02-25 15:27:41 -0500

    :sparkles: create gif custom hook

--- gif-app/src/components/GifGrid.tsx
+++ gif-app/src/components/GifGrid.tsx
@@ -2,24 +2,16 @@ import React, { FC, useEffect, useState } from 'react'
 import { IGifItem } from '../interfaces/gifItem'
 import { getGifs } from '../services/gif_service'
 import GifItem from './GifItem'
+import useFetchGifs from '../hooks/useFetchGifs';
 interface IGifGridProps {
     category: string
 }
 const GifGrid: FC<IGifGridProps> = ({ category }) => {
-    const [images, setimages] = useState<IGifItem[]>([])
-    const getGifsForComponent = async () => {
-        const gifs = await getGifs(category)
-        console.log(gifs)
-        setimages(gifs)
-    }
-    useEffect(() => {
-        getGifsForComponent()
-    }, [])
+    const { images, isLoading } = useFetchGifs(category);
     return (
         <>    <h2>
             {category} </h2>
             <div className='card-grid'>
-
                 <br />
                 {
                     images.map(
---
+++ gif-app/src/hooks/useFetchGifs.ts
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react"
+import { IGifItem } from "../interfaces/gifItem"
+import { getGifs } from "../services/gif_service"
+
+
+const useFetchGifs = (category: string) => {
+    const [images, setimages] = useState<IGifItem[]>([])
+    const [isLoading, setIsLoading] = useState(true)
+    const getGifsForComponent = async () => {
+        const gifs = await getGifs(category)
+        setimages(gifs)
+        setIsLoading(false)
+    }
+    useEffect(() => {
+        getGifsForComponent()
+    }, [])
+    return {
+        images,
+        isLoading
+    }
+}
+
+export default useFetchGifs
