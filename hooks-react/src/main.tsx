6df8c687b0d955e56bc77ec47c60118b79156c00
Juan Cevallos <cevallosjuanfrancisco@gmail.com>
2023-03-06 07:49:12 -0500

    :tada: add boostrap

--- hooks-react/src/main.tsx
+++ hooks-react/src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import './index.css'
+import HooksApp from './components/HooksApp';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+   <HooksApp></HooksApp>
   </React.StrictMode>,
 )
